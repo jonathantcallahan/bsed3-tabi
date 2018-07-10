18eb7961413ea4a20f191085d50d9f9ba480bd42
Jonathan <jtc7537@gmail.com>
2018-07-10 17:23:27 -0400

    generate pages

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,9 +2,6 @@ $( document ).ready(function(){
     
     const log = console.log;
     
-    // const reasons = [];
-    // const concerns = [];
-    
     const scrolling = {
         scroll: (hash, duration = 800) => {
             log(hash)
@@ -16,7 +13,7 @@ $( document ).ready(function(){
         eventATags: () => {
             $('a.jump').click(function(){
                 event.preventDefault();
-                if(this.hash === '#two-q' && sorting.reasons.length < 1){
+                if(this.hash === '#page-2' && sorting.reasons.length < 1){
                     //log('asdf')
                     $('#alert-1').attr('class','alert display')
                     return;
@@ -66,66 +63,63 @@ const sorting = {
                 log(sorting.reasons)
                 log(sorting.concerns)
             }
+        })
+        $('.answer-box').droppable('option','activeClass','highlight')
+
+        $('.selection').draggable({
+            snap: true,
+            distance: 5,
+            revert: 'invalid',
+            appendTo: function(event, ui){
+                console.log(event)
+                console.log(ui)
+            },
+            containment: 'window'
         })    
     },
 
 }
 
 sorting.eventDroppable();
-    // setTimeout(scroll.bind(null, '#interest-q'),3000)
 
+pages = 3;
+const createPages = {
+    pageHtml: {
+        reason_health: `
+            <section class='reasons-section' id='page-${pages}'>
+                <div>This will be what a generated page looks like</div>
+                <a class='jump' href='#page-${pages + 1}'>Continue</a>
+            </section>
+        `,
+        reason_animalW: `
+            <section class='reasons-section' id='page-${pages}'>
+                <div>This will be what a generated page looks like</div>
+                <a class='jump' href='#page-${pages + 1}'>Continue</a>
+            </section>
+        `
+    },
+    generate: () => {
+        $('a.reasons-create').click(function(){
+            log('pages ran')
+          if(sorting.reasons.length > 0){
+              log('create pages')
+              const three = $('#page-2')
+              sorting.reasons.forEach((e,i) => {
+                  log(e)
+                  log(createPages.pageHtml[e])
+                  three.after(createPages.pageHtml[e])
+                  pages = i + 3;
+                  log(pages)
+              })
+          }  
+        })
+    }
+}
 
-
-    // const sortItems = (e, answerId, question) => {
-    //     const indexT = e.indexOf(answerId) > -1
-    //     const index = e.indexOf(answerId)
-    //     if(question){
-    //         !indexT && e.push(answerId)
-    //     } else {
-    //         indexT && e.splice(index, 1)
-    //     }
-            
-    // // }
-
-    // $('.answer-box, .options').droppable({
-    //     accept: '.selection',
-    //     drop: function(event, ui) {
-    //         const answer = $(ui.draggable).attr('class')
-    //         const answerId = $(ui.draggable).attr('id')
-    //         const target = $(this).context.className
-    //         const section = target.indexOf('one') > -1
-    //         const question = target.indexOf('answer') > -1
-    //         log(section)
-    //         log(question)
-
-
-            
-    //         section ? 
-    //             sorting.sortItems(reasons, answerId, question) : 
-    //             sorting.sortItems(concerns, answerId, question)   
-            
-
-    //         log(answer)
-    //         log(target)
-    //         log(reasons)
-    //         log(concerns)
-    //         $('#alert-1').attr('class','alert')
-    //     }
-    // })
-
-    $('.answer-box').droppable('option','activeClass','highlight')
-
+createPages.generate()
     
-    $('.selection').draggable({
-        snap: true,
-        distance: 5,
-        revert: 'invalid',
-        appendTo: function(event, ui){
-            console.log(event)
-            console.log(ui)
-        },
-        containment: 'window'
-    })
+    
+    
 
     // $('.selection').click(function(){
     //     console.log($(this).parent().attr('class'))
