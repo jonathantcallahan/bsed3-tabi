211197fdc2ccf748bbd8c8ebb3c7837a5d257892
unknown <jtc7537@gmail.com>
2018-08-13 22:03:36 -0400

    dynamic pages

--- public/scripts/content-pages.js
+++ public/scripts/content-pages.js
@@ -7,7 +7,9 @@ const content = {
         $('body').on('click','div.dy-cl', function(){
             log('asdf')
             const currentPage = $(this).attr('pg')
+            $('div[pg=' + currentPage + '].dy-ex').find('i').attr('class','fas fa-plus dy-icon')
             $('div[pg=' + currentPage + '].dy-ex').attr('class','dy-content dy-cl')
+            $(this).find('i').attr('class','fas fa-minus dy-icon')
             $(this).attr('class','dy-content dy-ex')
         })
     }
