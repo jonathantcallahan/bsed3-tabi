9faec94e5f2e8021eab8d6ddff474223f3af92c2
unknown <jtc7537@gmail.com>
2018-08-26 23:09:07 -0400

    staging info server

--- controllers/html.js
+++ controllers/html.js
@@ -1,5 +1,3 @@
-import { dirname } from 'path';
-
 const path = require('path')
 
 module.exports = app => {
@@ -9,7 +7,11 @@ module.exports = app => {
     app.get('/drag', (req,res) => {
         res.sendFile(path.join(__dirname, './../public/html/drag.html'))
     })
-    app.get('/staging/:info', (req,res) => {
-        res.sendFile(path.join(__dirname, `./../public/html/${req.params.info}.html`))
+    app.get('/staging', (req,res) => {
+        res.sendFile(path.join(__dirname, './../public/html/staging.html'))
     })
+    app.get('/staging/:info', (req,res) => {
+        console.log(req.params.info)
+        res.sendFile(path.join(__dirname, `./../public/html/${req.params.info}.html`))
+    })    
 }
